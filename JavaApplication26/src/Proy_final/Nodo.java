ea79213b79e94b80e1218d11e8c8c405ed66544b
erick rodriguez <fenixazul96@hotmail.com>
2019-08-01 20:00:40 -0600

    nodo

---
+++ JavaApplication26/src/Proy_final/Nodo.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Proy_final;
+
+/**
+ *
+ * @author fenix
+ */
+public class Nodo {
+        public int dato;
+    public Nodo iz;
+    public Nodo Dr;
+
+    
+
+    public Nodo() {
+        
+        
+    }
+
+    public int getDato() {
+        return dato;
+    }
+
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
+
+    public Nodo getIz() {
+        return iz;
+    }
+
+    public void setIz(Nodo iz) {
+        this.iz = iz;
+    }
+
+    public Nodo getDr() {
+        return Dr;
+    }
+
+    public void setDr(Nodo Dr) {
+        this.Dr = Dr;
+    }
+
+  
+    public String toString() {
+        return "este es el dato "+ dato;
+    }
+    
+    
+}
